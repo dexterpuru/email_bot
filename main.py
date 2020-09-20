89fb985898eaade7275b23fcfc3906d10c785c63
Pratyaksh <pratyaksh2000saini@gmail.com>
2020-09-20 21:01:55 +0530

    using ssl, email library, and also attachment

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # import os
 import smtplib
+import imghdr
 from secrets import ps, add
+from email.message import EmailMessage
 
 # EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 # EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+msg = EmailMessage()
+msg['Subject'] = "Pratyaksh's email bot"
+msg['From'] = add
+msg['To'] = 'pratyaksh2000saini@gmail.com'
+msg.set_content('01001000 01100101 01101100 01101100 01101111 00100001')
 
+with open('anime1.jpg', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype='image',
+                   subtype=file_type, filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(add, ps)
-
-    subject = "Dinner this weekend?"
-    body = "Wanna grab dinner this weekend at Wendy's?"
-
-    msg = f'Subject: {subject}\n\n{body}'
-    smtp.sendmail(add, 'pratyaksh2000saini@gmail.com', msg)
+    smtp.send_message(msg)
